d8b978d84253d98c905a6f6100bf6923067ffde1
Georgi Katsarov <georgekatsarov95@gmail.com>
2023-02-11 15:55:19 +0200

    initial commit

---
+++ src/components/FirstSection.vue
@@ -0,0 +1,69 @@
+<template>
+    <section id="home" class="banner slide-bg relative z-0 pb-16 pt-32 overflow-hidden before:opacity-90 before:w-full before:h-full before:z-0 before:absolute before:top-0 before:left-0 h-auto lg:h-[800px]">
+			<!-- Container -->
+			<div class="container max-w-7xl w-full mx-auto md:px-6 lg:px-10 px-1">
+				<div class="row items-center flex flex-wrap -mr-1 -ml-1">
+					<!-- Content -->
+					<div class="res-margin relative z-20 w-full px-4 text-left max-w-full md:max-w-[58.333333%] lg:max-w-[50%] mb-8 md:mb-0 lg:mb-0">
+						<!-- Banner text -->
+
+							<h1 class="wow fadeInUp  text-white font-roboto font-bold  tracking-tight mb-6  leading-[1.2] text-4xl md:text-[3.75rem] lg:text-6xl text-tighter md:text-tight lg:text-tight text-center lg:text-left" data-wow-offset="10" data-wow-duration="1s" data-wow-delay="0s" style="visibility: visible; animation-duration: 1s; animation-delay: 0s; animation-name: fadeInUp;">
+								Get the new App for <br>iPhone &amp; Android
+							</h1>
+							
+							<p class="wow fadeInUp text-white mb-10 text-lg text-center lg:text-left leading-[1.75]" data-wow-offset="10" data-wow-duration="1s" data-wow-delay="0.3s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;">
+								Mauris ut fermentum orci. 
+								Pellentesque ultrices interdum ultrices. 
+								Aliquam erat volutpat. 
+								Mauris molestie aliquam hendrerit. 
+								Integer cursus neque eget auctor finibus, at enim euismod.
+							</p>
+
+							<div data-wow-offset="10" data-wow-duration="1s" data-wow-delay="0.6s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.6s; animation-name: fadeInUp;">
+								
+								<a href="#" class="py-2.5 px-2.5 md:px-4 lg:px-4 m-2 border-white border-solid border font-medium rounded inline-flex mr-2.5">
+									<BellIcon class="h-5 w-5 text-2xl mt-1 text-white font-normal inline-block mr-2.5" aria-hidden="true" /><p><span class=" block text-white font-bold text-lg">Google Play</span></p>
+								</a>
+								
+								<a href="#" class="py-2.5 px-2.5 md:px-4 lg:px-4 min-w-[150px] m-2 border-white border-solid border font-medium rounded inline-flex">
+									<BellIcon class="h-5 w-5 text-2xl mt-1 text-white font-normal inline-block mr-2.5" aria-hidden="true" /><p><span class=" block text-white font-bold text-lg">App Store</span></p>
+								</a>
+							
+							</div>
+
+					
+					</div>  
+					
+					<!-- Image -->
+					<div class="relative w-full z-20 max-w-full md:max-w-[41.666%] lg:max-w-[50%] px-4 lg:px-0">
+						<div class="banner-image-center wow fadeInUp relative my-0 mx-auto text-center max-w-full lg:max-w-[300px]" data-wow-offset="10" data-wow-duration="1s" data-wow-delay="0.3s" style="visibility: visible; animation-duration: 1s; animation-delay: 0.3s; animation-name: fadeInUp;">
+							<img src="slide-welcome.png">
+						</div>
+
+					</div>
+					
+				</div>
+				
+			</div>
+			
+			<!-- Bottom shape -->
+            <div class="bottom-shape absolute z-10 top-auto -bottom-1 left-0 right-0">
+                <svg viewBox="0 0 1920 310" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" class="svg replaced-svg">
+                    <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+                        <g transform="translate(0.000000, -554.000000)" fill="#fafbfe">
+                            <path d="M-3,551 C186.257589,757.321118 319.044414,856.322454 395.360475,848.004007 C509.834566,835.526337 561.525143,796.329212 637.731734,765.961549 C713.938325,735.593886 816.980646,681.910577 1035.72208,733.065469 C1254.46351,784.220361 1511.54925,678.92359 1539.40808,662.398665 C1567.2669,645.87374 1660.9143,591.478574 1773.19378,597.641868 C1848.04677,601.75073 1901.75645,588.357675 1934.32284,557.462704 L1934.32284,863.183395 L-3,863.183395"></path>
+                        </g>
+                    </g>
+                </svg>
+            </div>
+			
+            <div class="backstretch top-0 left-0 overflow-hidden m-0 p-0 -z-10 absolute w-screen" style="height: 800px">
+                <img class="absolute m-0 p-0 border-none w-screen max-h left-0 -top-4" src="slideshow-2.jpg">
+            </div>
+    </section>
+</template>
+
+
+<script setup>
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline';
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,92 @@
+<template>
+      <Disclosure as="nav" class="absolute w-full h-32 top-0 left-0 z-10" v-slot="{ open }">
+        <div class="mx-auto max-w-7xl px-8 md:px-20 pt-6 md:pt-8">
+          <div class="flex h-16 items-center justify-between">
+            <div class="flex items-center">
+              <div class="flex-shrink-0">
+                <img class="h-6 w-24" src="/logo-white@2x.png" alt="Your Company" />
+              </div>
+
+
+            </div>
+
+            <div class="hidden lg:block">
+
+              <div class="hidden md:block">
+                <div class="ml-10 flex items-baseline space-x-3">
+                  <a v-for="item in navigation" :key="item.name" :href="item.href" :class="[item.current ? 'underline ' : 'opacity-80 hover:opacity-100', 'decoration-1 underline-offset-4 py-2.5 px-4 rounded-md text-base font-medium text-left font-roboto text-white hover:text-white hover:underline']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</a>
+                  
+                  <a class="decoration-1 underline-offset-4 rounded-md text-base font-medium text-left text-white hover:text-white hover:underline opacity-80 hover:opacity-100">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-5 h-5 pt-1">
+                      <path fill-rule="evenodd" d="M9 3.5a5.5 5.5 0 100 11 5.5 5.5 0 000-11zM2 9a7 7 0 1112.452 4.391l3.328 3.329a.75.75 0 11-1.06 1.06l-3.329-3.328A7 7 0 012 9z" clip-rule="evenodd" />
+                    </svg>
+                    </a>
+                </div>
+              </div>
+            </div>
+
+            
+            <div class="-mr-6 flex lg:hidden">
+              <!-- Mobile menu button -->
+              <DisclosureButton class="inline-flex items-center justify-center rounded-md  text-white">
+                <span class="sr-only">Open main menu</span>
+                <Bars3Icon v-if="!open" class="block h-10 w-10" aria-hidden="true" />
+                <XMarkIcon v-else class="block h-10 w-10" aria-hidden="true" />
+              </DisclosureButton>
+            </div>
+          </div>
+        </div>
+  
+        <DisclosurePanel class="lg:hidden">
+          <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+            <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current="item.current ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+          </div>
+          <div class="border-t border-gray-700 pt-4 pb-3">
+            <div class="flex items-center px-5">
+              <div class="flex-shrink-0">
+                <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+              </div>
+              <div class="ml-3">
+                <div class="text-base font-medium leading-none text-white">{{ user.name }}</div>
+                <div class="text-sm font-medium leading-none text-gray-400">{{ user.email }}</div>
+              </div>
+              <button type="button" class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <span class="sr-only">View notifications</span>
+                <BellIcon class="h-6 w-6" aria-hidden="true" />
+              </button>
+            </div>
+            <div class="mt-3 space-y-1 px-2">
+              <DisclosureButton v-for="item in userNavigation" :key="item.name" as="a" :href="item.href" class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white">{{ item.name }}</DisclosureButton>
+            </div>
+          </div>
+        </DisclosurePanel>
+      </Disclosure>
+
+
+  </template>
+  
+  <script setup>
+  import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+  import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+  
+  const user = {
+    name: 'Tom Cook',
+    email: 'tom@example.com',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+  }
+  const navigation = [
+    { name: 'Home', href: '#', current: true },
+    { name: 'Features', href: '#', current: false },
+    { name: 'Screenshots', href: '#', current: false },
+    { name: 'Support', href: '#', current: false },
+    { name: 'Blog', href: '#', current: false },
+    { name: 'Pricing', href: '#', current: false },
+    { name: 'Contact', href: '#', current: false },
+  ]
+  const userNavigation = [
+    { name: 'Your Profile', href: '#' },
+    { name: 'Settings', href: '#' },
+    { name: 'Sign out', href: '#' },
+  ]
+  </script>
